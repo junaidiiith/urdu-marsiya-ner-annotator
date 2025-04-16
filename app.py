f1b71528af14e4836c013959158c4af7f0ac6e4a
Junaid Ali <junaid695683@gmail.com>
2025-04-16 21:49:04 +0200

    Minor fixes

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import streamlit as st
+from ner_annotator.auth import authenticate
+from settings import auth_file
+
 
 def run_pages(pages):
     pg = st.navigation(pages)
     pg.run()
 
+authenticate(auth_file=auth_file)
 
+if st.session_state['authentication_status']:
+    # print("Authentication status: Now", st.session_state['authentication_status'])
+    username = st.session_state['username']
+    name = st.session_state['name']  
+    roles = st.session_state['roles']
 
-# if st.session_state['authentication_status']:
-#     # print("Authentication status: Now", st.session_state['authentication_status'])
-#     username = st.session_state['username']
-#     name = st.session_state['name']  
-#     roles = st.session_state['roles']
+    pages = [
+        st.Page("app_pages/upload_and_tagging.py", title="New Upload and NER Tagging"),
+        st.Page("app_pages/reviewing.py", title="LLM NER Tags Reviewing"),
+    ]
 
-pages = [
-    st.Page("app_pages/upload_and_tagging.py", title="New Upload and NER Tagging"),
-    st.Page("app_pages/reviewing.py", title="LLM NER Tags Reviewing"),
-]
-
-run_pages(pages)
+    run_pages(pages)
